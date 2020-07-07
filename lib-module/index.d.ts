566e0d0b1446a2c0e08164799c32af3f01a98043
zouhang <zouhang@didiglobal.com>
2020-07-07 19:48:33 +0800

    build: build code

---
+++ lib-module/index.d.ts
@@ -0,0 +1,4 @@
+import { App, ComponentOptions } from 'vue';
+export declare const useCreate: (Component: ComponentOptions, app?: App<any> | undefined, options?: any) => any;
+declare const install: (app: App) => void;
+export default install;
---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import commonjs from 'rollup-plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+
+const external = [
+  'vue'
+]
+
+const plugins = [
+  terser(),
+  commonjs(),
+  resolve(),
+  typescript(),
+]
+
+const input = './src/index.ts'
+
+export default [
+  {
+    input,
+    output: {
+      format: 'esm',
+      dir: 'lib-module'
+    },
+    plugins,
+    external,
+  },
+  {
+    input,
+    output: {
+      format: 'cjs',
+      dir: 'lib'
+    },
+    plugins,
+    external,
+  }
+]
